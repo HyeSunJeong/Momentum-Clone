93d7c38c2046552c19252665bbc9c7ca5f8f1e55
HyeSunJeong <zaqxswcde88@naver.com>
2020-07-17 03:26:20 +0900

    commit

--- gretting.js
+++ gretting.js
@@ -1,18 +1,12 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
-    greeting = document.querySelector(".js-greetings");
+    greeting = document.querySelector("js-greetings");
 
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
-function handleSubmit(event) {
-    event.preventDefault();
-    const currentValue = input.value;
-}
-
 function askForName() {
     form.classList.add(SHOWING_CN);
-    form.addEventListener("submit", handleSubmit);
 }
 
 function paintGreeting(text) {
@@ -26,12 +20,12 @@ function loadName() {
     if (currentUser === null) {
         askForName();
     } else {
-        paintGreeting(currenUser);
+        paintGreeting(currentUser);
     }
 }
 
-function init(){
-
+function init() {
+    loadName();
 }
 
 init();
