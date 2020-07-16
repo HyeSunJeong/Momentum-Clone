2d92d044ea6b1a2bf03f197e17114b04c85d5f1a
HyeSunJeong <zaqxswcde88@naver.com>
2020-07-17 01:29:23 +0900

    commit

---
+++ clock.js
@@ -0,0 +1,15 @@
+const clockContainer = document.querySelector(".js-clock"),
+ clockTitle = clockContainer.querySelector("h1");
+
+function getTime(){
+    const date = new Date();
+    const minutes = date.getMinutes();
+    const hours = date.getHours();
+    clockTitle.innerText = `${hours}:${minutes}`;
+}
+
+function init() {
+
+}
+
+init();
